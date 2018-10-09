d7dfeddaec20f4814c068c5dc0060650e7faac4a
Igor Shturmov <shturmov.i@gmail.com>
2018-10-09 20:07:17 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import Header from "./components/header/Header";
 
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Photo from "./components/photo/Photo";
+import Video from "./components/video/Video";
+import Documents from "./components/documents/Documents";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Header />
+        <Fragment>
+            <Header />
+
+            <Route exact path="/" component={Home} />
+            <Route exact path="/photo" component={Photo} />
+            <Route exact path="/video" component={Video} />
+            <Route exact path="/documents" component={Documents} />
+            <Route exact path="/about" component={About} />
+        </Fragment>
       </BrowserRouter>
     );
   }
---
+++ src/components/documents/Documents.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Documents extends React.Component{
+    render() {
+        return <div>Hello from Documents!</div>;
+    }
+}
